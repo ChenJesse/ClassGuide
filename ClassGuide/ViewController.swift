4d9d4ec5d3ae49353f0f2bd73256449e8008210a
Jesse Chen <jmc622@cornell.edu>
2016-03-22 14:39:22 -0400

    Initial Commit

---
+++ ClassGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClassGuide
+//
+//  Created by Jesse Chen on 3/22/16.
+//  Copyright © 2016 Jesse Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
