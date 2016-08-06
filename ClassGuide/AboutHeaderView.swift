ff82bf124e25d50373c7c58978d80ff66245924d
Jesse Chen <jmc622@cornell.edu>
2016-08-05 20:47:05 -0700

    Link to Cornell major page

--- ClassGuide/AboutHeaderView.swift
+++ ClassGuide/AboutHeaderView.swift
@@ -10,5 +10,9 @@ import UIKit
 
 class AboutHeaderView: UICollectionReusableView {
 
-    
+    @IBOutlet weak var majorButton: UIButton!
+    let majorURL = NSURL(string: "https://www.cs.cornell.edu/undergrad/csmajor")
+    @IBAction func majorButtonClicked(sender: UIButton) {
+        UIApplication.sharedApplication().openURL(majorURL!)
+    }
 }
